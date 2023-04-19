befc4763e016d033f2e307f8b88515ecc9c735f5
Zwiicker <hickzwicker@gmail.com>
2023-04-19 19:05:32 -0300

    Css melhorado e adicionado mais infos nos detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,9 @@ function loadPokemonItens(offset, limit) {
 
 loadPokemonItens(offset, limit)
 
+function goHome() {
+  window.location.href = "index.html";
+}
 /*loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtdRecordsWithNexPage = offset + limit
@@ -64,17 +67,38 @@ async function search() {
   }
 
   function displayPokemon(pokemon) {
-    const pokemonListHTML = `
-      <div>
-        <img src="${pokemon.sprites.front_default}" />
-        <h2>${pokemon.name}</h2>
-        <p>Height: ${pokemon.height}m</p>
-        <p>Weight: ${pokemon.weight}kg</p>
-        <p>Abilities: ${pokemon.abilities.map((a) => a.ability.name).join(", ")}</p>
-      </div>
-    `;
-    pokemonList.innerHTML = pokemonListHTML;
-  }
-
-  //click
+     fetch(pokemon.species.url)
+      .then((response) => response.json())
+      .then((species) => {
+        fetch(species.evolution_chain.url)
+          .then((response) => response.json())
+           {
+            const typeList = pokemon.types.map((t) => t.type.name).join(", ");
+            const statList = `
+              <ul>
+                <li>HP: ${pokemon.stats[0].base_stat}</li>
+                <li>Attack: ${pokemon.stats[1].base_stat}</li>
+                <li>Defense: ${pokemon.stats[2].base_stat}</li>
+                <li>Special Attack: ${pokemon.stats[3].base_stat}</li>
+                <li>Special Defense: ${pokemon.stats[4].base_stat}</li>
+                <li>Speed: ${pokemon.stats[5].base_stat}</li>
+              </ul>
+            `;
+            const pokemonListHTML = `
+              <div>
+                <img src="${pokemon.sprites.front_default}" />
+                <p>Height: ${pokemon.height}m</p>
+                <p>Weight: ${pokemon.weight}kg</p>
+                <p>Type(s): ${typeList}</p>
+                <p>Abilities: ${pokemon.abilities.map((a) => a.ability.name).join(", ")}</p>
+                <p>Base Stats:</p>
+                ${statList}
+                <button onclick="goHome()">Go Home</button>
+              </div>
+            `;
+            pokemonList.innerHTML = pokemonListHTML;
+          };
+      })}
+    
   
+
